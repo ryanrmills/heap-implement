7eab0fb259ec0b1417fea92f9ffe5a2ff729d8d5
ryanrmills <ryanroymills@gmail.com>
2026-05-26 20:05:04 -0700

    first commit

--- src/Heap.java
+++ src/Heap.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A min-heap. 
  * 
@@ -22,5 +25,84 @@
  *  - hold private instance variables
  */
 public class Heap {
+    private static List<Integer> numList;
 
+    public Heap(){
+        numList = new ArrayList<>();
+    }
+
+    public void add(int num){
+        //sift up
+        numList.add(num);
+        siftUp(num, numList.size() - 1);
+    }
+
+    public int pop(){
+        int popped = numList.get(0);
+        int last = numList.get(numList.size() - 1);
+
+        numList.remove(0);
+        numList.add(0, last);
+        
+        //sift down
+        siftDown(last, 0);
+        return popped;
+    }
+
+    public int peek(){
+        if (numList.size() == 0) throw new IllegalStateException("The heap is empty.");
+        return numList.get(0);
+    }
+
+    public int size(){
+        return numList.size();
+    }
+
+    private static void siftUp(int num, int index){
+        int parentIndex = findParentIndex(index) > 0 ? findParentIndex(index) : 0;
+        int currentIndex = index;
+        while (numList.get(parentIndex) > num){
+            //switch?
+            int original = numList.get(parentIndex); //[0, 2, 3, 2]
+            numList.set(parentIndex, num);
+            numList.set(currentIndex, original);
+            currentIndex = findParentIndex(currentIndex);
+        }
+    }
+
+    private static void siftDown(int num, int index){
+        int currentIndex = index;
+        int currentLeft = findChildIndex(index)[0];
+        int currentRight = findChildIndex(index)[1];
+        while (numList.get(currentLeft) < num || numList.get(currentRight) < num){
+            if (numList.get(currentLeft) < num && numList.get(currentLeft) < numList.get(currentRight)){
+                int original = numList.get(currentLeft);
+                numList.set(currentLeft, num);
+                numList.set(currentIndex, original);
+                currentIndex = currentLeft;
+                if (findChildIndex(currentIndex)[0] >= numList.size() - 1) return;
+            } else if (numList.get(currentRight) < num && numList.get(currentRight) < numList.get(currentLeft)){
+                int original = numList.get(currentRight);
+                numList.set(currentRight, num);
+                numList.set(currentIndex, original);
+                currentIndex = currentRight;
+                if (findChildIndex(currentIndex)[1] >= numList.size() - 1) return;
+            }
+        }
+    }
+
+    private static int findParentIndex(int index){
+        double var1 = Math.floor(index / 2);
+        double var2 = index / 2;
+        int parentIndex = var1 < var2 ?  (int) var1 : (int) var1 - 1;
+        return parentIndex;
+    }
+
+    private static int[] findChildIndex(int index){
+        return new int[]{(2 * index + 1), (2 * index + 2)};
+    }
+
+    public String toString(){
+        return numList.toString();
+    }
 }
--- src/HeapTest.java
+++ src/HeapTest.java
@@ -3,4 +3,94 @@ import org.junit.Test;
 
 public class HeapTest {
 
+    @Test
+    public void addBuildsExpectedHeapShape() {
+        Heap heap = new Heap();
+
+        heap.add(7);
+        heap.add(3);
+        heap.add(5);
+        heap.add(1);
+        heap.add(9);
+        heap.add(2);
+
+        assertEquals("[1, 3, 2, 7, 9, 5]", heap.toString());
+        assertEquals(6, heap.size());
+        assertEquals(1, heap.peek());
+    }
+
+    @Test
+    public void peekDoesNotRemoveFront() {
+        Heap heap = new Heap();
+
+        heap.add(12);
+        heap.add(4);
+        heap.add(7);
+
+        assertEquals(4, heap.peek());
+        assertEquals(3, heap.size());
+        assertEquals(4, heap.peek());
+        assertEquals(3, heap.size());
+    }
+
+    @Test
+    public void popReturnsValuesInAscendingOrder() {
+        Heap heap = new Heap();
+
+        heap.add(8);
+        heap.add(4);
+        heap.add(10);
+        heap.add(1);
+        heap.add(6);
+        heap.add(14);
+        heap.add(13);
+        heap.add(3);
+
+        assertEquals(1, heap.pop());
+        assertEquals(3, heap.pop());
+        assertEquals(4, heap.pop());
+        assertEquals(6, heap.pop());
+        assertEquals(8, heap.pop());
+        assertEquals(10, heap.pop());
+        assertEquals(13, heap.pop());
+        assertEquals(14, heap.pop());
+        assertEquals(0, heap.size());
+    }
+
+    @Test
+    public void heapHandlesNegativeAndDuplicateValues() {
+        Heap heap = new Heap();
+
+        heap.add(-2);
+        heap.add(5);
+        heap.add(-2);
+        heap.add(0);
+        heap.add(5);
+
+        assertEquals(-2, heap.peek());
+        assertEquals(-2, heap.pop());
+        assertEquals(-2, heap.pop());
+        assertEquals(0, heap.pop());
+        assertEquals(5, heap.pop());
+        assertEquals(5, heap.pop());
+        assertEquals(0, heap.size());
+    }
+
+    @Test
+    public void mixedAddPopSequenceMaintainsHeapProperty() {
+        Heap heap = new Heap();
+
+        heap.add(10);
+        heap.add(4);
+        heap.add(15);
+
+        assertEquals(4, heap.pop());
+
+        heap.add(2);
+        heap.add(8);
+
+        assertEquals("[2, 8, 10, 15]", heap.toString());
+        assertEquals(2, heap.peek());
+        assertEquals(4, heap.size());
+    }
 }
